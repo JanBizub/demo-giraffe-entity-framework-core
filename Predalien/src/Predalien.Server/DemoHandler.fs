21faebcdcafc596f2d3d72a1bfda939bb5cff7b9
Jan Bizub <jan.bizub@outlook.com>
2022-10-19 10:53:08 +0200

    rm redundand code

--- Predalien/src/Predalien.Server/DemoHandler.fs
+++ Predalien/src/Predalien.Server/DemoHandler.fs
@@ -1,7 +1,6 @@
 [<RequireQualifiedAccess>]
 module DemoHandler
 
-open System
 open Microsoft.AspNetCore.Http
 open Giraffe
 open Predalien.Database
@@ -9,17 +8,14 @@ open Predalien.Database
 let getArticles () =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         task {
-            use db = ctx.GetService<Predalien.Database.PredalienDbContext>()
+            use db = ctx.GetService<PredalienDbContext>()
 
-            let students () = query {
+            let articles () = query {
                 for article in db.Articles do
                     select article
             }
             
-            let result = 
-                students ()
-
-            return! json result next ctx
+            return! json (articles ()) next ctx
         }
 
 
@@ -27,7 +23,7 @@ let postArticle () =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         task {
             let! article = ctx.BindModelAsync<Article>()
-            use db = ctx.GetService<Predalien.Database.PredalienDbContext>()
+            use db = ctx.GetService<PredalienDbContext>()
 
             db.Add article |> ignore
             db.SaveChanges () |> ignore
